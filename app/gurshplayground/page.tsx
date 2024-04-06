a1c06662eb07ccc324a9d9dff341917abc054caa
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-06 16:39:14 -0700

    Add Gursh playground
    Co-Authored-By: dzyfer <dzyfer@dzyfer.com>

---
+++ app/gurshplayground/page.tsx
@@ -0,0 +1,9 @@
+export default async function GurshPlayground() {
+  return (
+    <>
+      <h1>Welcome to Gursh Playground!</h1>
+      <p>This is your starting point for building awesome React applications.</p>
+      <button className='icon recycle'></button>
+    </>
+  )
+}
