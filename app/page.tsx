c6385155d60caff046a9d374886a93a6e177d954
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-07 03:51:57 -0700

    Move blob list

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 import { ImageUploader } from '@/components/ImageUploader'
 import { LoadingBar } from '@/components/LoadingBar'
 import { ReceptacleSelector } from '@/components/ReceptacleSelector'
-import { list } from '@vercel/blob'
 
 export default async function Home() {
-  const response = await list()
   return (
     <>
       <ImageUploader />
       <ReceptacleSelector />
       <LoadingBar />
-      {response.blobs.map((blob) => (
-        <a key={blob.downloadUrl} href={blob.downloadUrl}>
-          {blob.pathname}
-        </a>
-      ))}
     </>
   )
 }
