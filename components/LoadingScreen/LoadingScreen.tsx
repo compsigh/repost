894def90242fd97c3181e5e11ce133842c263024
dzyfer <95269077+dzyfer@users.noreply.github.com>
2024-04-06 20:50:34 -0700

    LoadingScreen component

---
+++ components/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export function LoadingScreen() {
+  return (
+    <div id="loading-screen">
+      <div id="loading-left"></div>
+      <div id="loading-right"></div>
+    </div>
+  )
+}
