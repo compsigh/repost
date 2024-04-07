b3caa1fefe8dcda74c5f9849024f5781e84b0ffa
Andrew <adiep@dons.usfca.edu>
2024-04-07 09:35:57 -0700

    Update API call to return as JSON object string

--- functions/handle-upload.ts
+++ functions/handle-upload.ts
@@ -4,17 +4,29 @@ import { put } from '@vercel/blob'
 import { getVisionResponse } from './openai'
 
 export async function handleUploadServerAction(
-  prevState: any, formData: FormData
-) {
-  const rawFormData = {
-    file: formData.get('file') as File
-  }
-
-  const blob = await put(
-    rawFormData.file.name,
-    rawFormData.file,
-    { access: 'public' }
-  )
-
-  return await getVisionResponse(blob.url)
+export async function handleUploadServerAction() {
+  return await getVisionResponse('https://images.unsplash.com/photo-1528825871115-3581a5387919?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFuYW5hfGVufDB8fDB8fHww')
 }
+
+// export async function handleUploadServerAction(
+//   prevState: any, formData: FormData
+// ) {
+//   const rawFormData = {
+//     file: formData.get('file') as File
+//   }
+//   console.log('rawFormData', rawFormData)
+
+//   try {
+//     const blob = await put(
+//       rawFormData.file.name,
+//       rawFormData.file,
+//       { access: 'public' }
+//     )
+//     console.log('blob', blob)
+//     return await getVisionResponse(blob.url)
+//   } catch (e) {
+//     console.error('Error uploading file', e)
+//     return { message: 'Error uploading file' }
+//   }
+
+// }
--- functions/openai.ts
+++ functions/openai.ts
@@ -8,7 +8,7 @@ export async function getVisionResponse(imageUrl: string) {
     messages: [
       {
         role: 'system',
-        content: 'You are a helpful program that assists users in identifying the compostability and recyclability of the objects in their images. Respond to the user with a terse explanation of which receptacle the object belongs in, with brief decomposition instructions as necessary.'
+        content: 'You are a helpful program that assists users in identifying the compostability and recyclability of the objects in their images. If it does not look like trash, say you are unsure. You must respond to the user in JSON format. The first object, named Receptacle, should contain which of the three receptacles that the trash belongs in: the trash, compost, or recycle. If it belongs in more than one, comma separate the receptacle. The second object, named Disposal, contains a step by step way of decomposition instructions as necessary based on the type of receptacles(s) given. The third object, named Repost, contains alternate ways the user can reuse or utilize that trash for other activities.'
       },
       {
         role: 'user',
@@ -23,6 +23,12 @@ export async function getVisionResponse(imageUrl: string) {
       }
     ]
   })
+
+  let JSON_response: string = response.choices[0].message.content ?? ''
+  JSON_response = JSON_response.slice(7)
+  JSON_response = JSON_response.slice(0, -3)
+  const parsed_response = JSON.parse(JSON_response)
+  console.log(parsed_response)
 
   return { message: response.choices[0].message.content }
 }
