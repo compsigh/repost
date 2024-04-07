028cc57f46e266df3d50dbb7503de47bcc24fdbb
dzyfer <95269077+dzyfer@users.noreply.github.com>
2024-04-07 06:47:17 -0700

    created Onboarding page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
+import Image from 'next/image'
 import { ImageUploader } from '@/components/ImageUploader'
 import { LoadingBar } from '@/components/LoadingBar'
 import { ReceptacleSelector } from '@/components/ReceptacleSelector'
+import wordmark from '@/public/svgs/wordmark.svg'
+import { Button } from '@/components/Button'
 
 export default async function Home() {
   return (
     <>
-      <ImageUploader />
+      <Onboarding />
+      {/* <ImageUploader />
       <ReceptacleSelector />
-      <LoadingBar />
+      <LoadingBar /> */}
+    </>
+  )
+}
+
+function Onboarding() {
+  return (
+    <>
+      <div id='wordmark'>
+        <Image
+          src={wordmark}
+          alt="Gursh Playground Wordmark"
+          width={320}
+          height={140}
+        />
+      </div>
+      <h1 id='title'>Not sure where to put your <span id="compost">compost?</span></h1>
+      <br />
+      <h2 id='subtitle'>Now you know.</h2>
+      <Button id='get-started' kind='cta'>Sign in</Button>
     </>
   )
 }
